ff8bd3a3fc5566257ff38b5d9dce4d4d17fb90d0
Luis - PC <Luis - PC@DevPC>
2022-09-20 22:55:39 -0500

    Correccion calculo del cuadrado

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8080/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido! Esta aplicación nos ayuda a indentificar el funcionamiento del prcoeso de CI-CD.");
+        return String.format("Bienvenido! Esta aplicación nos ayuda a indentificar el funcionamiento del proceso de CI-CD.");
     }    
 
     //Se ejecuta con http://localhost:8080/hola o http://localhost:8080/hola?nombre=John
